7f3c7e47bdb3467ba74f55e954a75db317e857fa
barroscamila <camilagdbarros@gmail.com>
2023-09-27 20:52:56 -0300

    update DOM

--- javascript/dom/calc.js/index.js
+++ javascript/dom/calc.js/index.js
@@ -3,8 +3,10 @@ const root = document.querySelector(':root')
 const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
 
+// o  array abaixo serve para especficar quais caracteres podem ser inseridos (teclas permitidas)
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+// selecionar todos os elementos de classe charkey (botoes da calculadora) e inserir um evento neles (click)
 document.querySelectorAll('.charKey').forEach(function (charKeyBtn) {
     charKeyBtn.addEventListener('click', function () {
         const value = charKeyBtn.dataset.value // acessar o data-value
@@ -12,21 +14,26 @@ document.querySelectorAll('.charKey').forEach(function (charKeyBtn) {
     })
 })
 
+// evento relacinado ao botao 'C' (click)
 document.getElementById('clear').addEventListener('click', function () {
     input.value = ''
     resultInput.value = ''
     input.focus() // focar/colocar o cursor no input
 })
 
-input.addEventListener('keydown', function (ev) { //evento KEYDOWN = quando uma tecla é acionada/apertada
-    ev.preventDefault()
+// evento relacionado ao clicar nas teclas (evento keydown)
+input.addEventListener('keydown', function (ev) { 
+    //evento KEYDOWN = quando uma tecla é acionada/apertada
+    
+    ev.preventDefault() // evitar que a tecla seja inserida no input
+
     if (allowedKeys.includes(ev.key)) { // ev.key -> a tecla acionada
         input.value += ev.key
         return // encerrar a função
     } 
 
     if (ev.key === 'Backspace') { // tecla de deletar
-        input.value = input.value.slice(0, -1) // posicao inicial a -1 (penultimo caracter)
+        input.value = input.value.slice(0, -1) // posicao inicial a -1 (penultimo caracter), ou seja, ira "apagar" o ultimo caractere
     }
 
     if (ev.key === 'Enter') { // tecla enter
@@ -34,10 +41,12 @@ input.addEventListener('keydown', function (ev) { //evento KEYDOWN = quando uma 
     }
 })
 
+// evento relacionado a tecla '=' (click)
 document.getElementById('equal').addEventListener('click', calculate)
 
+// funcao de calculo dos caracteres inseridos na calculadora
 function calculate() {
-    resultInput.value = 'ERROR'
+    resultInput.value = 'ERROR'// setar esse valor padrao, ira aparecer quando o eval nao funcionar
     resultInput.classList.add('error')
 
     const result = eval(input.value) // avalia um código JS representado como string e retorna seu resultado final (tomar cuidado para utilizar!! pode ler codigos inseguros que o usuario inserir)
@@ -46,6 +55,7 @@ function calculate() {
     resultInput.classList.remove('error')
 }
 
+// evento relacioano ao botao 'Copy'
 document.getElementById('copyToClipboard').addEventListener('click', function (ev) {
     const button = ev.currentTarget // elemento que acionou o evento
     if (button.innerText === 'Copy') {
@@ -58,6 +68,7 @@ document.getElementById('copyToClipboard').addEventListener('click', function (e
     }
 })
 
+// evento relacionado a troca de tema (botao switch theme)
 document.getElementById('themeSwitcher').addEventListener('click', function () {
     if (main.dataset.theme === 'dark') {
         root.style.setProperty('--bg-color', '#f1f5f9')
