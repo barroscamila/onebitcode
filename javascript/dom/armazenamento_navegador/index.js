280fd2a0f9cf9b88a28eb19d2be901626eb317b0
barroscamila <camilagdbarros@gmail.com>
2023-10-23 20:09:22 -0300

    finished DOM 2.0

--- javascript/dom/armazenamento_navegador/index.js
+++ javascript/dom/armazenamento_navegador/index.js
@@ -1,17 +1,17 @@
-// Para verificar os dados armazenadoss: DevTools --> Application
-
+// Para verificar os dados armazenadoss: DevTools --> Application --> Storage --> Local Storage, Session Storage, Cookies
 
 //Session Storage
 document.getElementById('sessionBtn').addEventListener('click', function () {
     const input = document.getElementById('session')
-    window.sessionStorage.setItem('info', input.value)
+    window.sessionStorage.setItem('info', input.value) // pode ser apenas sessionStorage, sem o window
     input.value = ''
 })
 
 document.getElementById('readSession').addEventListener('click', function () {
-    const info = sessionStorage.getItem('info')
+    const info = sessionStorage.getItem('info') // acessar a key 'info'
     alert('A informação salva é: ' + info)
 })
+
 
 //Local Storage
 document.getElementById('localBtn').addEventListener('click', function () {
@@ -25,13 +25,14 @@ document.getElementById('readLocal').addEventListener('click', function () {
     alert('O texto salvo na Local Storage é: ' + t)
 })
 
+
 //Cookies
 document.getElementById('cookieBtn').addEventListener('click', function () {
     const input = document.getElementById('cookie')
     // cookieName=value; expires=UTCStringDate; path=/; FORMATO DO COOKIE 
-    const cookie = 'info=' + input.value + ';'
-    const expiration = 'expires=' + new Date(2023, 4, 27) + ";"
-    const path = 'path=/' // a '/' representa todas as pastas da pagina
+    const cookie = 'info=' + input.value + ';' //cookieName
+    const expiration = 'expires=' + new Date(2023, 10, 27) + ';' //expires
+    const path = 'path=/;' // a '/' representa todas as pastas da pagina
     document.cookie = cookie + expiration + path 
     input.value = ''
     console.log(document.cookie)
@@ -39,10 +40,10 @@ document.getElementById('cookieBtn').addEventListener('click', function () {
 
 document.getElementById('cookie2Btn').addEventListener('click', function () {
     const input = document.getElementById('cookie2')
-    const cookie = 'text=' + input.value + ';'
-    const expiration = 'expires=' + new Date(2023, 8, 9) + ";"
-    const path = 'path=/'
-    document.cookie = cookie + expiration + path 
+    const cookie2 = 'text=' + input.value + ';'
+    const expiration2 = 'expires=' + new Date(2024, 11, 1) + ';'
+    const path2 = 'path=/;'
+    document.cookie = cookie2 + expiration2 + path2 
     input.value = ''
     console.log(document.cookie)
 })
