50daa6770fcb34e6433807cd448761cc7ea5b352
barroscamila <camilagdbarros@gmail.com>
2023-11-15 13:35:36 -0300

    stuying WEBPACK

--- javascript/js_moderno/webpack/src/index.js
+++ javascript/js_moderno/webpack/src/index.js
@@ -1,8 +1,9 @@
 import dayjs from 'dayjs'
+import './styles/index.css'
 
 alert(`Hoje é: ${dayjs().format("DD/MM/YYYY")}`)
 
 
 // npx webpack -> empacota o código, incluindo tudo que precisa para ele funcionar (inclusive o node_modules)
 
-// OBS: npm install -> instala todos os node modules apagados anteriormente
+// npm install -> instala todos os node modules apagados anteriormente (pelo json ele verifica o que tinha antes)
--- javascript/js_moderno/webpack/webpack.config.js
+++ javascript/js_moderno/webpack/webpack.config.js
@@ -1,29 +1,8 @@
-// Arquivo padrão de configuração do webpack
-// É preciso utilizar CommomJS por causa do Node
+    // #AULA - CONFIGURANDO O WEBPACK
+    // Arquivo padrão de configuração do webpack
+    // É preciso utilizar CommomJS por causa do Node
 
-// -----------------------------------------------
-
-// AULA - PUGLINS
-
-
-// AULA - LOADERS (permitem ler o CSS e incluir no arquivo de saída -> style-loader e css-loader) 
-
-module.exports = {
-    entry: {
-        index: './src/index.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [{
-            // test: 
-        }]
-    }
-}
-
-
-/*
-    // AULA - CONFIGURANDO O WEBPACK
-
+    /*
     const path = require('path')
 
     module.exports = {
@@ -32,11 +11,55 @@ module.exports = {
             hello: './src/hello.js' // mais de uma entrada
         },
 
-        mode: "production",   // especificando o mode "development" ou "production"
+        mode: "development",   // especificando o mode "development" ou "production"
 
         output: { 
             path: path.resolve(__dirname, 'public'), // especificando o caminho da saída, utilizando o modulo PATH do proprio node
             filename: "[name].bundle.min.js" // especificando nome do arquivo de saida (o "[name]" serve para diferenciar os arquivos de saida, quando tem mais de uma entrada)
         }
     }
-*/
+    */
+
+// -----------------------------------------------
+
+    // AULA - LOADERS 
+    // Permitem ler o CSS e incluir no arquivo de saída -> npm install --save-dev style-loader e css-loader
+
+    /*
+    module.exports = {
+        entry: {
+            main: './src/index.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [{
+                test: /\.css$/, // em qual arquivo serão utilizados os loaders
+                use: ['style-loader', 'css-loader']  // quais loaders utilizar
+            }]
+        }
+    }
+    */
+
+// -----------------------------------------------
+
+    // AULA - PLUGINS 
+    // Permite gerar um arquivo de saida .css separado do .js -> npm install --save-dev mini-css-extract-plugin 
+
+    const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+    module.exports = {
+        entry: {
+            main: './src/index.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [{
+                test: /\.css$/, 
+                use: [miniCssExtractPlugin.loader, 'css-loader'] 
+            }]
+        },
+        plugins: [
+            new miniCssExtractPlugin()
+        ]
+    }
+
