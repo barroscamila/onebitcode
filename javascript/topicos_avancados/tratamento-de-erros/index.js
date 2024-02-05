bde20e87541e0802c2f2a76883074c12aabeca59
barroscamila <camilagdbarros@gmail.com>
2024-02-05 20:48:04 -0300

    studying errors

---
+++ javascript/topicos_avancados/tratamento-de-erros/index.js
@@ -0,0 +1,30 @@
+function sum(a, b) {
+    const firstNumber = Number(a)
+    const secondsNumber = Number(b)
+
+    if (isNaN(firstNumber) || isNaN(secondsNumber)) {
+        throw new Error('arguments must be two numbers')
+    }
+
+    // quando o erro acontece, a função para de ser executada
+
+    return firstNumber + secondsNumber
+}
+
+// o try + catch pode ser utilizado para lidar com o erro, uma forme de contornar o erro ("plano B")
+// o finaly
+try {
+    console.log(sum(2,9))
+    console.log(sum(true, 14))
+    // console.log(sum(undefined, 22))
+    console.log(sum(18, "0"))
+    console.log(sum(39, null))
+    // console.log(sum(13, "zero"))
+} catch (error) {
+    console.log('An error ocorred!')
+    // console.log(error) // mostra as informacoes do erro, padrão do JS
+    console.log(error.message) // mostra apenas a mensagem do erro (trhow new Error())
+} finally {
+    console.log("Calculations finished!")
+}
+
