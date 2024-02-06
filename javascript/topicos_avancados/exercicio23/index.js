a400817f5d21a48bbf63cd52146d94d5a54e74b9
barroscamila <camilagdbarros@gmail.com>
2024-02-06 20:56:52 -0300

    styding Regex

---
+++ javascript/topicos_avancados/exercicio23/index.js
@@ -0,0 +1,53 @@
+function validateEmail(email) {
+    if (!email.match(/\w{2,}@[a-zA-z]{2,}\.[a-zA-Z]{2,}/)) { // xx@xx.xx
+        const err = new Error('E-mail inválido.') 
+        err.input = 'email'
+        throw err
+        // ao inves de lançar diretamente o erro com o throw, neste caso foi criado o erro, manipulado e depois lançado
+    }
+}
+
+function validatePassword(password) {
+    if (
+        password.length < 8 || 
+        !password.match(/[a-z]/) ||
+        !password.match(/[A-Z]/) ||
+        !password.match(/[0-9]/) ||
+        !password.match(/[^a-zA-Z0-9\s]/) // caractere que nao seja letra minuscula, maiuscula e numero de 0 a 9, ou seja, um caractere especial --> ^ serve para negar
+    ) {
+        const err = new Error('Senha inválida.')
+        err.input = 'password'
+        throw err
+    }
+}
+
+function resetFormStyles() {
+    Object.entries(userInputs).forEach(([key, value]) => {
+        value.classList.remove('success', 'error')
+        document.querySelector(`#${key}-error`).textContent = ''
+    })
+}
+
+const userInputs = {
+    name: document.querySelector('#name'),
+    email: document.querySelector('#email'),
+    password: document.querySelector('#password')
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault() 
+    resetFormStyles()
+    try {
+        userInputs.name.classList.add('success')
+        validateEmail(userInputs.email.value)
+        userInputs.email.classList.add('success')
+        validatePassword(userInputs.password.value)
+        userInputs.password.classList.add('success')
+    } catch (err) {
+        userInputs[err.input].classList.add('error')
+        document.querySelector(`#${err.input}-error`).textContent = err.message
+    }
+})
+
